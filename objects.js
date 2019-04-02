1c5ffb9ed0569bd1e03f727aa413006afbcf22bd
hoodielive <hoodielive@gmail.com>
2019-04-01 22:29:12 -0400

    objects.. I missed you baby muah

---
+++ objects.js
@@ -0,0 +1,8 @@
+let day1 = {
+  squirrel: false, 
+  events: ["work", "touched tree", "pizza", "running"]
+}
+
+console.log(day1.squirrel)
+console.log(day1.wolf) // undefined
+console.log(day1.wolf) // false
