f1ce4a28f78dc01deb08266fe4ba1960b337e052
hoodielive <hoodielive@gmail.com>
2019-04-09 10:06:38 -0400

    create your own shit

--- object-oriented-javascript/create_your_own_datatype.js
+++ object-oriented-javascript/create_your_own_datatype.js
@@ -2,18 +2,14 @@ function Person(firstname, lastname) {
   this.firstname = firstname; 
   this.lastname = lastname; 
 
-  Object.defineProperty(this, "firstname", {
-    get: function () { return firstname;}
-  }) // oooo this is so ugly! 
-
-  this.greet = function(name) {
-      return "Hello " + name + ". My name is" + firstname; 
-    }
+Person.prototype.greet = function(name) {
+    return "Hello " + name + ". My name is" + firstname; 
   }
+}
 
-let obj = createPerson('King', 'Immortalis')
-let obj2 = createPerson('King', 'Decroid')
-let obj3 = new Object(); 
+let obj = new Person('King', 'Immortalis')
 
-console.log(obj,obj2,obj3)
-console.log(obj.greet === obj2.greet)
+let obj2 = new Person('King', 'Decroid')
+
+console.log(obj,obj2)
+console.log(obj.greet === obj2.greet) // now you are true!! 
