cbf36a671e7b6b275f183f16d725f6efca549916
hoodielive <hoodielive@gmail.com>
2019-04-12 11:05:58 -0400

    adding review

---
+++ review-es6/let-const-review-lol-2019.js
@@ -0,0 +1,10 @@
+function getClothing(isCold) {
+	if (isCold) {
+		let freezing = "grab a jacket"
+	}
+	else {
+		console.log(freezing) // error, freezing is not defined
+	}
+}
+
+getClothing(false)
