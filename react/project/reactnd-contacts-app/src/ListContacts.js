1116ce81bd97caa5d7f8e6b7c0b9da8fadda0a0c
hoodielive <hoodielive@gmail.com>
2019-04-12 16:50:55 -0400

    ListContacts component

--- react/project/reactnd-contacts-app/src/ListContacts.js
+++ react/project/reactnd-contacts-app/src/ListContacts.js
@@ -5,7 +5,11 @@ class ListContacts extends Component {
 		console.log('Props', this.props)
 		return( 
 			<ol className='contact-list'> 
-
+				{this.props.contacts.map((contact) => (
+					<li key={contacts.id}>
+						{contact.name}
+					</li>
+				))}
 			</ol>
 		)
 	}
