da4adb791e457811c9ace2c48f2d72b1efd659c2
hoodielive <hoodielive@gmail.com>
2019-04-29 20:04:33 -0400

    functional javascript

---
+++ functional-javascript/calculate.js
@@ -0,0 +1,3 @@
+let percentValue = 5; 
+let calculation = (value) => { return value/100 * ( 100 + percentValue ) }
+let rewrite_calculation = (value, percentValue) => { return value/100 * (100 + percentValue)}
