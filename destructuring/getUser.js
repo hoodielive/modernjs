27f8ffb99819843bf3aa07202ec0cff95f8122ab
hoodielive <hoodielive@gmail.com>
2019-12-08 08:15:15 -0500

    destructuring

---
+++ destructuring/getUser.js
@@ -0,0 +1,12 @@
+function getUser() {
+	return {
+		name: 'Tzyurash',
+		handle: '@tzyurash',
+		location: 'Garden of Eden'
+	}
+}
+
+var { name, handle, location } = getUser()
+console.log(name)
+console.log(handle)
+console.log(location)
