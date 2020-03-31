1be75548b48bbcba238376619c833843722170e5
hoodielive <hoodielive@gmail.com>
2020-03-31 14:30:00 -0400

    Javascript you are the most mercurial of all languages

---
+++ types/typesofstuff.js
@@ -0,0 +1,27 @@
+name = "Emitt"
+num = 44
+afloat = 3.14
+obj = {
+	name: "Becky",
+	age: 43
+}
+
+asymbol = Symbol('Fortnite');
+
+function diamondIntheRough() {}
+
+class Forget {
+	constructor(name, age) {
+		this.name = name
+		this.age = age
+	}
+}
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof afloat)
+console.log(typeof obj)
+console.log(typeof asymbol)
+console.log(typeof diamondIntheRough)
+console.log(typeof Forget)
+
