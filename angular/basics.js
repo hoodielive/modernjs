f34564b190c67d1b00baf65188f286374cb1e44e
hoodielive <hoodielive@gmail.com>
2020-11-16 22:34:13 -0500

    I am being lazy

--- angular/basics.js
+++ angular/basics.js
@@ -1,10 +1,13 @@
 const httpOptions = { timeout: 2000, cache: { age: 2 } };
 
 const { cache: { age } } = httpOptions;
+console.log(httpOptions);
 
 const timeouts = [1000, 2000, 3000];
+console.log(timeouts);
 
 const [shortTimeout, mediumTimeout] = timeouts;
+console.log(timeouts);
 
 function randomPonyInRace() 
 {
