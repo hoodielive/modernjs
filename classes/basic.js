ca2535fb49f872823e8973d2c258d5a49022b8e0
hoodielive <hoodielive@gmail.com>
2020-12-14 00:10:58 -0500

    good night.

---
+++ classes/basic.js
@@ -0,0 +1,24 @@
+class Color
+{
+	constructor(r=0, g=0, b=0)
+	{
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+
+	get rgb()
+	{
+		return "rgb(" + this.r + ", " + this.g + this.b + ")";
+	}
+
+	set rgb(value)
+	{
+	
+	}
+
+	toString()
+	{
+
+	}
+}
