b2650130dcdb0335176099f697580effcbeb6632
hoodielive <hoodielive@gmail.com>
2021-10-28 00:44:32 -0400

    Man.. I've lost it.

---
+++ creating_new_objects.js
@@ -0,0 +1,19 @@
+// Creating Objects with new
+
+var o = new Object(); 
+let a = new Array(); 
+let d = new Date();
+let r = new Map();
+
+console.log(o);
+console.log(a);
+console.log(d);
+console.log(r);
+
+o.orin = "Erick"
+o.denn = "Dennville"
+console.log(o.orin);
+console.log(o.denn);
+
+a.Today = ('a')
+console.log(a.Today)
