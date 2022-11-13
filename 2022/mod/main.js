112718d3884b5e9e510b07dd7f7ba18f11cddae7
hoodielive <hoodielive@gmail.com>
2022-11-13 13:27:32 -0500

    Ok.

---
+++ 2022/mod/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let name = "Osa";
+
+function init() {
+	if (true) {
+		let email = "osa@gmail.com";
+	}
+	document.getElementById("output").innerHTML = email;
+}
+
+console.log(name);
+
+init();
