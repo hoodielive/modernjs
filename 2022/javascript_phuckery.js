12b4371ee333b13f06fe481ec4a4ad07cb23baf5
hoodielive <hoodielive@gmail.com>
2022-11-30 19:34:59 -0500

    Javascript phuckery

---
+++ 2022/javascript_phuckery.js
@@ -0,0 +1,7 @@
+let know = false; // you are a boolean and you were declared with the 'let' keyword
+
+console.log(typeof know);
+
+know = 434566; // now you are a phucking number.. sigh!
+
+console.log(typeof know);
