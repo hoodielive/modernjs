b70f8ad2b6a696a6aa1a4b47d63cb04f4161fbb8
hoodielive <hoodielive@gmail.com>
2022-12-10 12:00:44 -0500

    Helping my classs

---
+++ 2022/basics.js
@@ -0,0 +1,30 @@
+// Javascript Types
+
+// Numbers
+a = 3; // integer
+b = 2.3; // float
+
+// Strings
+astring = "this is a string.";
+
+// Boolean
+abool = true;
+anotherBool = false;
+
+is_undefined = undefined;
+
+is_null = null;
+
+an_obj = {
+	name: "Something",
+	age: "something else",
+};
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof astring);
+console.log(typeof abool);
+console.log(typeof anotherBool);
+console.log(typeof is_undefined);
+console.log(typeof is_null);
+console.log(typeof an_obj);
---
+++ 2022/operators.js
@@ -0,0 +1,60 @@
+// Numbers
+
+// Addition
+let num = 1 + 1; // 2
+
+// Subtraction
+let sub = 4 - 9;
+
+// Multiplication
+let multiply = 4 * 9;
+
+// Division
+let divide = 4 / 9;
+
+// Modulus
+let modulo = 30 % 4; // 2
+
+// Bit Shifiing
+let bit_shift = 1 << 2; // Binary Math                      256  128  64  32  16  8  4  2  1
+
+// PEMDAS Parantheses, Exponents, Multiplication, Division, Addition and Subtraction
+let result = 1 + 3 - 2 * 4;
+
+// Booleans
+let is_employed = true;
+let is_fired = !false;
+
+is_employed = !true;
+
+// Strings
+let joinedString = "hello" + " " + "world";
+
+// Comparison operators
+let a = 0;
+let b = 3;
+let first_comp = 1 === a;
+
+// === exact match, equality operator - checks for equality
+// !== inequality ! (negation operator)
+
+// comparison operators
+console.log(1 <= 10);
+console.log(1 >= 10);
+
+// concatenation operator
+let sample = "1, 2, " + 3;
+console.log(sample);
+console.log(typeof sample);
+
+console.log("Hello, " + ["Irete", "tu", "!"]);
+
+console.log(undefined == null);
+console.log(null === undefined);
+
+console.log(13 + !0);
+console.log("13" + !0);
+
+// String methods.
+console.log("Oyeku Meji".charAt(6));
+console.log("Oyeku Meji".substring(0, 5));
