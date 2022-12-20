206b15484c8a3af7d75b8f449c36a3ea6b05759c
hoodielive <hoodielive@gmail.com>
2022-12-19 22:41:27 -0500

    Constructor functions

--- 2022/_JourneysinCode/constructor_functions.js
+++ 2022/_JourneysinCode/constructor_functions.js
@@ -1,8 +1,19 @@
+let log = console.log
+
 function Cat(name)
 {
   this.name = name
   this.sound = "Meow"
 }
 
-let cat = Cat("Osa")
-cat.sound
+let cat = new Cat("Osa")
+
+Cat.prototype.speak = function()
+{
+  log(this.sound)
+}
+
+cat.speak()
+cat.constructor // Returns type function
+
+log(cat instanceof Cat) // Returns a boolean
