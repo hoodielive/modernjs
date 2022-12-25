281ea14b3ff13ec939be8b400bbdf2a7b8c96d37
hoodielive <hoodielive@gmail.com>
2022-12-25 10:56:03 -0500

    I hate you

--- 2022/_OduProject/app.js
+++ 2022/_OduProject/app.js
@@ -1,4 +1,5 @@
 let log = console.log
+let element = undefined;
 
 const fire = ["ogbe", "irosun", "ogunda", "otura"]
 const water = ["oyeku", "owonrin", "osa", "irete"]
@@ -20,3 +21,24 @@ if (oduFire === OduEnumFire.ogunda)
 {
   log(`${oduFire}`)
 }
+
+fire.forEach(odu => {
+  if (odu === "ogunda")
+  {
+    element = "fire" 
+    log(`${odu} is in the East and is of the element ${element}.`)
+  }
+})
+
+log('We will begin with the primary odu. What is the primary Odu?');
+let answer = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+reader.question("Please enter the primary odu.", value => {
+  log(`The value of primary odu is ${value}`)
+})
+
+reader.close();
