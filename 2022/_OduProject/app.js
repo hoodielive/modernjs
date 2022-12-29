d1b06f1080c9300b92d4fce7698f65094f7eecbd
hoodielive <hoodielive@gmail.com>
2022-12-28 22:41:05 -0500

    More ideas. Nothing special lol.

--- 2022/_OduProject/app.js
+++ 2022/_OduProject/app.js
@@ -42,3 +42,43 @@ reader.question("Please enter the primary odu.", value => {
 })
 
 reader.close();
+
+let whichOdu = "";
+
+function cycle()
+{
+  return {
+    odus: [
+      { odu: 'ogbe', element: 'fire', direction: 'east'},
+      { odu: 'oyeku', element: 'water', direction: 'west'},
+      { odu: 'iwori', element: 'air', direction: 'north'},
+      { odu: 'idi', element: 'earth', direction: 'south'},
+      { odu: 'irosun', element: 'fire', direction: 'east'},
+      { odu: 'owonrin', element: 'water', direction: 'west'},
+      { odu: 'obara', element: 'air', direction: 'north'},
+      { odu: 'okanran', element: 'earth', direction: 'south'},
+      { odu: 'ogunda', element: 'fire', direction: 'east'},
+      { odu: 'osa', element: 'water', direction: 'west'},
+      { odu: 'ika', element: 'air', direction: 'north'},
+      { odu: 'oturupon', element: 'earth', direction: 'south'},
+      { odu: 'otura', element: 'fire', direction: 'east'},
+      { odu: 'irete', element: 'water', direction: 'west'},
+      { odu: 'ose', element: 'air', direction: 'north'},
+      { odu: 'ofun', element: 'earth', direction: 'south'},
+    ],
+    get returnOdus()
+    {
+      return this.odus.filter(odu => odu.returned);
+    },
+    returnOdu(odu) 
+    {
+      odu.returned = ! odu.returned;
+    },
+  }
+}
+if (this.returnOdus.length === whichOdu)
+{
+  if (this.cycle.returnOdus[0]['ogbe'] === this.cycle.returnOdus['ogbe']['east']) {
+    console.log("Ogbe is the king of reads!");
+  }
+}
