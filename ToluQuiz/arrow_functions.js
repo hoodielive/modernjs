89ca3571bf3070cc9de25de2dfe368e12dd76e44
hoodielive <hoodielive@gmail.com>
2022-12-29 15:36:28 -0500

    Prep for Tolu Session functions

--- ToluQuiz/arrow_functions.js
+++ ToluQuiz/arrow_functions.js
@@ -9,3 +9,31 @@ function UiComponent()
         this.handleClick();
     })
 }
+
+var sum = function(a, b) {
+    return a + b;
+}
+console.log(sum(2, 3));
+
+let sums = a => a;
+
+console.log(sums(4));
+
+let ball = function(x, y, vx, vy ) 
+{
+    this.x = x; 
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.dt = 25;
+
+    setInterval(() =>  {
+        this.x += vx;
+        this.y += vy;
+        console.log(this.x, this.y);
+    }, this.dt);
+}
+
+let baller = new ball(0, 0, 10000, 10000);
+
+console.log(baller);
