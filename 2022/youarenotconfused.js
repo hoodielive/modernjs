c528246d57cf65f18f550797cd243284cdc26b88
hoodielive <hoodielive@gmail.com>
2023-01-08 12:22:20 -0500

    Tolu exercise

---
+++ 2022/youarenotconfused.js
@@ -0,0 +1,18 @@
+class User
+{
+  constructor(username, email)
+  {
+    this.username = username;
+    this.email = email;
+  }
+
+  returnNewEmail()
+  {
+    return this.newEmail;
+  }
+
+  newEmail(email)
+  {
+    
+  }
+}
