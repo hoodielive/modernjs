88bd1b9d051cd5f7389089ed757ace8065bdb21b
hoodielive <hoodielive@gmail.com>
2023-03-17 13:01:50 -0400

    Lets write a error handling.

--- 2023/Node/server.js
+++ 2023/Node/server.js
@@ -19,6 +19,14 @@ let server = http.createServer((req, res) => {
 
     // Set response content. 
     res.write('<html><body><p>This is the home page.</p></body></html>')
+    res.end()
+  }
+
+  if (req.url == '/product') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+
+    res.write('<html><body><p>This is the product page.</p></body></html>')
+    res.end()
   }
 })
 
